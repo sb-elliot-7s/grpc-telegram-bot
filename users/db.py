0a25c990a580bc4de2d254300fecc83f313cfc35
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-02-23 12:26:30 +0300

    add db.py to users service

---
+++ users/db.py
@@ -0,0 +1,9 @@
+import motor.motor_asyncio
+
+from configs import get_configs
+
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    f'mongodb://{get_configs().mongo_host}:{get_configs().mongo_port}'
+)
+
+database = client.database
