1aa9fa377ca68dc904f15f125835ecba999730cf
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-02-26 11:06:54 +0300

    add schemas to finance service

---
+++ finance/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+
+
+class TickerSchema(BaseModel):
+    currency: str
+    day_high: float = Field(alias='dayHigh')
+    day_low: float = Field(alias='dayLow')
+    fifty_day_average: float = Field(alias='fiftyDayAverage')
+    last_price: float = Field(alias='lastPrice')
+    last_volume: float = Field(alias='lastVolume')
+    market_cap: float = Field(alias='marketCap')
+    open_: float = Field(alias='open')
+    previous_close: float = Field(alias='previousClose')
+    quote_type: str = Field(alias='quoteType')
+    shares: int
+    timezone: str
+    year_change: float = Field(alias='yearChange')
+    year_high: float = Field(alias='yearHigh')
+    year_low: float = Field(alias='yearLow')
+
+
+class NewsSchema(BaseModel):
+    title: str
+    publisher: str
+    link: str
+    provider_publish_time: int = Field(alias='providerPublishTime')
+    type_: str = Field(alias='type')
+    related_tickers: list[str] = Field(alias='relatedTickers')
