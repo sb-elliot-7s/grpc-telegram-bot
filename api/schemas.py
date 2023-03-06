97142490b518ecd2835721832c3dbe856e838d1c
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-06 18:48:25 +0300

    change schema in api service: username +optional

--- api/schemas.py
+++ api/schemas.py
@@ -2,20 +2,20 @@ from datetime import datetime
 
 from pydantic import BaseModel, Field
 
+from utils import Int_Or_None, Str_Or_None
+
 
 class UserSchema(BaseModel):
     user_id: int = Field(alias='_id')
-    username: str
-    first_name: str | None
-    last_name: str | None
+    username: Str_Or_None
+    first_name: Str_Or_None
+    last_name: Str_Or_None
     date_created: datetime
 
     class Config:
-        json_encoders = {
-            datetime: lambda x: x.strftime('%Y:%m:%d %H:%M')
-        }
+        json_encoders = {datetime: lambda x: x.strftime('%Y:%m:%d %H:%M')}
 
 
 class Options(BaseModel):
-    skip: int | None
-    limit: int | None
+    skip: Int_Or_None
+    limit: Int_Or_None
