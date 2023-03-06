d556af34f7652a8c6f8bf01abd0e9bb69443e8da
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-06 14:22:46 +0300

    rename

--- users/db.py
+++ users/db.py
@@ -2,8 +2,6 @@ import motor.motor_asyncio
 
 from configs import get_configs
 
-client = motor.motor_asyncio.AsyncIOMotorClient(
-    f'mongodb://{get_configs().mongo_host}:{get_configs().mongo_port}'
-)
+client = motor.motor_asyncio.AsyncIOMotorClient(f'mongodb://{get_configs().mongo_host}:{get_configs().mongo_port}')
 
 database = client.database
