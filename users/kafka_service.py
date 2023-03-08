ef94b1f7404e472dde98fc9d3fd5d6d469dda715
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-08 10:10:04 +0300

    refactoring kafka service

--- users/kafka_service.py
+++ users/kafka_service.py
@@ -1,5 +1,4 @@
 import json
-from dataclasses import dataclass
 
 from aiokafka import AIOKafkaConsumer
 
@@ -7,23 +6,27 @@ from repositories import UserRepositories
 from schemas import UserSchema
 
 
-@dataclass
 class KafkaService:
-    repositories: UserRepositories
-    server: str
-
-    async def consume(self, topic: str):
-        consumer = AIOKafkaConsumer(
-            topic,
-            bootstrap_servers=self.server,
+    def __init__(self, repositories: UserRepositories, server: str, topic: str):
+        self.repositories = repositories
+        self.server: str
+        self.topic = topic
+        self.consumer = AIOKafkaConsumer(
+            self.topic,
+            bootstrap_servers=server,
             group_id='user-group',
             value_deserializer=lambda x: json.loads(x)
         )
-        await consumer.start()
+
+    async def consume(self):
+        await self.consumer.start()
         try:
-            async for message in consumer:
+            async for message in self.consumer:
                 if not await self.repositories.get_user(user_id=message.value.get('user_id')):
                     user = UserSchema(**message.value)
                     await self.repositories.save_user(user=user.to_mongo_obj())
         finally:
-            await consumer.stop()
+            await self.consumer.stop()
+
+    async def stop_consume(self):
+        await self.consumer.stop()
