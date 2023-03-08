367ce3976b98afe46b91454c71ef0bb205a45f76
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-08 10:06:25 +0300

    add repositories protocol

---
+++ users/protocols/repositories_protocol.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class UserRepositoriesProtocol(ABC):
+    @abstractmethod
+    async def get_user(self, user_id: int):
+        pass
+
+    @abstractmethod
+    async def get_users(self, limit: int = 20, skip: int = 0):
+        pass
+
+    @abstractmethod
+    async def count_of_users(
+            self, filters: dict,
+            skip: int | None = None,
+            limit: int | None = None
+    ):
+        pass
+
+    @abstractmethod
+    async def save_user(self, user: dict): pass
