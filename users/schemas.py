9ce51be22b78b1181f6fc7e6483527bfa9d843fe
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-08 10:08:50 +0300

    add new schema to query params in request

--- users/schemas.py
+++ users/schemas.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from fastapi import Query
 from pydantic import BaseModel
 
 
@@ -16,10 +17,32 @@ class UserSchema(BaseModel):
         }
 
     def to_mongo_obj(self):
-        return {
-            '_id': self.user_id,
-            'username': self.username,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'date_created': self.date_created
-        }
+        return {'_id': self.user_id, **self.dict(exclude={'user_id'})}
+
+    @classmethod
+    def from_mongo_obj(cls, mongo_user: dict):
+        return {'user_id': mongo_user.get('_id'), **mongo_user}
+
+
+class Options(BaseModel):
+    skip: int | None
+    limit: int | None
+
+
+class QueryParams(Options):
+    date_created: str | None
+    operator_date_created: str | None
+
+    @classmethod
+    def as_query(
+            cls, skip: int | None = Query(None),
+            limit: int | None = Query(None),
+            date_created: str | None = Query(None),
+            operator_date_created: str | None = Query(None)
+    ):
+        return cls(
+            skip=skip,
+            limit=limit,
+            date_created=date_created,
+            operator_date_created=operator_date_created
+        )
