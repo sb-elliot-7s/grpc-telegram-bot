123ee99cb35cf25675e6701a3d0883c3d6215f2c
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-08 10:27:19 +0300

    remove deps.py

--- api/deps.py
+++
@@ -1,8 +0,0 @@
-import motor.motor_asyncio
-
-from configs import get_configs
-
-client = motor.motor_asyncio.AsyncIOMotorClient(f'mongodb://{get_configs().mongo_host}:{get_configs().mongo_port}')
-
-database = client.database
-user_collection = database.users
