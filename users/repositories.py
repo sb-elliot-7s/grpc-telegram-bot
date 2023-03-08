ef406f8d665b87e536ec33e33d138fa7de644017
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-08 10:11:06 +0300

    realize repositories protocol, some fix

--- users/repositories.py
+++ users/repositories.py
@@ -2,13 +2,29 @@ from dataclasses import dataclass
 
 from motor.motor_asyncio import AsyncIOMotorCollection
 
+from protocols.repositories_protocol import UserRepositoriesProtocol
+from schemas import Options, UserSchema
+
 
 @dataclass
-class UserRepositories:
+class UserRepositories(UserRepositoriesProtocol):
     collection: AsyncIOMotorCollection
+
+    async def get_users(self, limit: int = 20, skip: int = 0):
+        return [
+            UserSchema.from_mongo_obj(user) async for user in self.collection
+            .find()
+            .skip(skip)
+            .limit(limit)
+        ]
+
+    async def count_of_users(self, filters: dict, skip: int | None = None, limit: int | None = None):
+        return await self.collection \
+            .count_documents(filter=filters, **Options(skip=skip, limit=limit).dict(exclude_none=True))
 
     async def save_user(self, user: dict):
         _ = await self.collection.insert_one(document=user)
 
     async def get_user(self, user_id: int):
-        return await self.collection.find_one({'_id': user_id})
+        user = await self.collection.find_one({'_id': user_id})
+        return UserSchema.from_mongo_obj(mongo_user=user) if user else None
