c82a78420420793d8de3941e4714c17e68763ac0
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-08 10:12:00 +0300

    add response configuration file

---
+++ users/response_configuration.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+from fastapi import status
+
+from schemas import UserSchema
+
+
+def get_common_options(
+        *, status_code: int = status.HTTP_200_OK,
+        model: Any,
+        path: str = ''
+) -> dict:
+    return {
+        'status_code': status_code,
+        'response_model': model,
+        'path': f'/users{path}'
+    }
+
+
+response_conf = {
+    'users': {**get_common_options(model=list[UserSchema])},
+    'detail_user': {**get_common_options(model=UserSchema, path='/{user_id}')},
+    'count': {**get_common_options(model=int, path='/count')}
+}
