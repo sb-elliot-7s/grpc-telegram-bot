373a656f969dfe308545c6027e93c4fbec063b64
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-08 10:07:37 +0300

    add lru_cache on function get_configs

--- users/configs.py
+++ users/configs.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from pydantic import BaseSettings
 
 
@@ -11,5 +13,6 @@ class Configs(BaseSettings):
         env_file_encoding = 'utf-8'
 
 
+@lru_cache
 def get_configs() -> Configs:
     return Configs()
