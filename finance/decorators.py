36895e520ca64972cf5ab4911f190e692b7a47db
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-09 14:25:30 +0300

    modified decorators: add cache function for many tickers data

--- finance/decorators.py
+++ finance/decorators.py
@@ -1,22 +1,62 @@
 import json
+from dataclasses import dataclass, field
 
 import aioredis
+from aioredis import Redis
 
-from configs import get_configs
 
+@dataclass
+class RedisDecorator:
+    host: str
+    port: int
+    redis: Redis = field(init=False)
 
-def cache(ex: int, type_: str | None = 'finance'):
-    def decorator(func):
-        async def wrapper(*args, **kwargs):
-            redis = aioredis.from_url(f'redis://{get_configs().redis_host}:{get_configs().redis_port}')
-            key = f"{type_}:{kwargs.get('ticker')}"
-            result = await redis.get(key)
-            if not result:
-                data = await func(*args, **kwargs)
-                await redis.set(name=key, value=json.dumps(obj=data, default=str), ex=ex)
-                return data
-            return json.loads(result)
+    def __post_init__(self):
+        self.redis = aioredis.from_url(f'redis://{self.host}:{self.port}')
 
-        return wrapper
+    def cache(self, ex: int = 60):
+        def decorator(func):
+            async def wrapper(*args, **kwargs):
+                key = kwargs.get('ticker')
+                result = await self.redis.get(key)
+                if not result:
+                    data = await func(*args, **kwargs)
+                    await self.redis.set(name=key, value=json.dumps(obj=data, default=str), ex=ex)
+                    return data
+                return json.loads(result)
 
-    return decorator
+            return wrapper
+
+        return decorator
+
+    async def _save_tickers_to_cache(self, data: list, ex: int):
+        for value in data:
+            await self.redis.set(
+                name=value.get('ticker').lower(),
+                value=json.dumps(value, default=str),
+                ex=ex
+            )
+
+    def cache_many_tickers(self, ex: int = 60):
+        def decorator(func):
+            async def wrapper(*args, **kwargs):
+                keys: list[str] = kwargs.get('tickers')
+                count_of_tickers: int = len(keys)
+                cache: list[str | None] = [await self.redis.get(name=key) for key in keys]
+                result = [x for x in cache if x is not None]
+                if not len(result):
+                    data = await func(*args, **kwargs)
+                    await self._save_tickers_to_cache(data=data, ex=ex)
+                    return data
+                cached_tickers: list[dict] = [json.loads(ticker) for ticker in result]
+                ticker_symbols_in_cache: list[str] = [t.get("ticker").lower() for t in cached_tickers]
+                if len(cached_tickers) != count_of_tickers:
+                    not_in_cache_ticker: list[str] = list(set(keys) - set(ticker_symbols_in_cache))
+                    data = await func(*args, tickers=not_in_cache_ticker)
+                    await self._save_tickers_to_cache(data=data, ex=ex)
+                    return cached_tickers + data
+                return cached_tickers
+
+            return wrapper
+
+        return decorator
