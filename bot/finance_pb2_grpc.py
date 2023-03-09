a0971d249f940a777fa29ef4ceec089abd3462f0
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-09 14:41:40 +0300

    regenerated grpc file

--- bot/finance_pb2_grpc.py
+++ bot/finance_pb2_grpc.py
@@ -24,6 +24,11 @@ class FinanceStub(object):
                 request_serializer=finance__pb2.TickerRequest.SerializeToString,
                 response_deserializer=finance__pb2.NewsResponse.FromString,
                 )
+        self.GetTickersResponse = channel.unary_unary(
+                '/Finance/GetTickersResponse',
+                request_serializer=finance__pb2.TickerRequest.SerializeToString,
+                response_deserializer=finance__pb2.TickersResponse.FromString,
+                )
 
 
 class FinanceServicer(object):
@@ -41,6 +46,12 @@ class FinanceServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def GetTickersResponse(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_FinanceServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -53,6 +64,11 @@ def add_FinanceServicer_to_server(servicer, server):
                     servicer.GetNewsResponse,
                     request_deserializer=finance__pb2.TickerRequest.FromString,
                     response_serializer=finance__pb2.NewsResponse.SerializeToString,
+            ),
+            'GetTickersResponse': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetTickersResponse,
+                    request_deserializer=finance__pb2.TickerRequest.FromString,
+                    response_serializer=finance__pb2.TickersResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -95,5 +111,22 @@ class Finance(object):
         return grpc.experimental.unary_unary(request, target, '/Finance/GetNewsResponse',
             finance__pb2.TickerRequest.SerializeToString,
             finance__pb2.NewsResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetTickersResponse(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Finance/GetTickersResponse',
+            finance__pb2.TickerRequest.SerializeToString,
+            finance__pb2.TickersResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
