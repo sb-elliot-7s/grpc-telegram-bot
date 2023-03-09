2b319fb1dc63a824df40374f9748665105cf9bbf
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-09 14:40:35 +0300

    add command: /tickers

--- bot/constants.py
+++ bot/constants.py
@@ -5,21 +5,27 @@ import aiogram.utils.markdown as fmt
 
 class Constants(str, Enum):
     START_TEXT = 'Hello {username}, write /help for more information'
-    
+
     HELP_TEXT = fmt.text(
         fmt.text(
             fmt.bold('Write command'),
-            fmt.text('/news and any ticker name to watch news \\(ex\\. /news aapl\\)')
+            fmt.text('/news and any ticker symbol to watch news \\(ex\\. /news aapl\\)')
         ),
         fmt.text(
-            fmt.bold('Write ticker name'),
+            fmt.bold('Write ticker symbol'),
             fmt.text('for financial information \\(ex\\. intc or qcom\\)')
+        ),
+        fmt.text(
+            fmt.bold('Write command'),
+            fmt.text('/tickers and tickers symbols \\(ex\\. /tickers aapl intc msft amd\\)')
         ),
         sep='\n\n'
     )
+    MUST_BE_ONE_SYMBOL = 'You must pass one ticker symbol'
 
 
 class Commands(str, Enum):
     start = 'start'
     help = 'help'
     news = 'news'
+    tickers = 'tickers'
