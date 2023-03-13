2a95795f78f94c8b9bf972df95a606046e8bca53
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-13 22:55:57 +0300

    add enum Topic for kafka: user and save_email

--- users/configs.py
+++ users/configs.py
@@ -1,6 +1,12 @@
+from enum import Enum
 from functools import lru_cache
 
 from pydantic import BaseSettings
+
+
+class Topic(str, Enum):
+    USER = 'user'
+    SAVE_EMAIL = 'save_email'
 
 
 class Configs(BaseSettings):
