b23980e900b3c901b42ce1ab9de9c2c348d41491
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-14 14:18:29 +0300

    add email_command

---
+++ financial_statement/email_command.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from protocols.command_protocols import CommandDecoratorProtocol
+from protocols.email_service_protocol import EmailServiceProtocol
+
+
+@dataclass
+class EmailGrpcCommand(CommandDecoratorProtocol):
+    email_service: EmailServiceProtocol
+
+    async def execute(self):
+        await self.__send_email()
+        return await self.command.execute()
+
+    async def __send_email(self):
+        await self.email_service.send_to_email()
