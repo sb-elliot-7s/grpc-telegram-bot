9dc43a2a80dc0ecaee6ff21fa3d355302ea7970f
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-14 14:14:46 +0300

    add schemas.py

--- financial_statement/schemas.py
+++ financial_statement/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from configs import get_configs
+
 
 class FinancialStatementSchema(BaseModel):
     symbol: str
@@ -10,3 +12,14 @@ class FinancialStatementResponseSchema(BaseModel):
     symbol: str
     pdf: bytes
     year: str
+
+
+class EmailDataSchema(BaseModel):
+    pdf: bytes
+    text: str
+    to_user: str
+    year: str
+    from_user: str = get_configs().from_user
+    email_host: str = get_configs().email_host
+    email_port: int = get_configs().email_port
+    email_password: str = get_configs().email_password
