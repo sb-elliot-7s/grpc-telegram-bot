7e56810fc5e1d64c8faf16696074ef4338786af2
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-15 13:22:01 +0300

    add base_url, api_token and exp_min in configs class

--- finance/configs.py
+++ finance/configs.py
@@ -6,6 +6,11 @@ class Configs(BaseSettings):
     redis_port: int
     debug: bool
 
+    base_url: str
+    api_token: str
+
+    ticker_exp_minute: int
+
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
