2915f82c4748201d579482528b92ade4633b9713
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-15 13:27:57 +0300

    change FinanceService() to APIFinanceService()

--- finance/app.py
+++ finance/app.py
@@ -3,19 +3,26 @@ import asyncio
 import grpc
 
 import finance_pb2_grpc as pb2_grpc
+from cache_finance_service import CacheFinanceService
 from configs import get_configs
-from finance_service import FinanceService, CacheFinanceService
+from finance_service_from_api import APIFinanceService
+# from finance_service_yahoo import FinanceService, CacheFinanceService
 from news_service import NewsService
 from servicer import FinanceServicer
 
 
-def get_finance_service(debug: bool):
-    return FinanceService() if debug else CacheFinanceService(service=FinanceService())
+# def get_finance_service(debug: bool):
+#     return FinanceService() if debug else CacheFinanceService(service=FinanceService())
+
+
+def get_api_service(debug: bool):
+    return APIFinanceService() if debug else CacheFinanceService(service=APIFinanceService())
 
 
 async def run_server():
     server = grpc.aio.server(compression=grpc.Compression.Gzip)
-    finance_service = get_finance_service(debug=get_configs().debug)
+    # finance_service = get_finance_service(debug=get_configs().debug)
+    finance_service = get_api_service(debug=get_configs().debug)
     pb2_grpc.add_FinanceServicer_to_server(
         servicer=FinanceServicer(
             finance_service=finance_service, news_service=NewsService()
