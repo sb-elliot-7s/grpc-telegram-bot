f2d55ad3ef628a792d5e7d4924c51c77de884e16
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-18 17:10:17 +0300

    refactoring get_financial_statement()

--- bot/grpc_reports_client.py
+++ bot/grpc_reports_client.py
@@ -14,13 +14,16 @@ class GRPCReportsClient(FinancialStatementServiceProtocol):
     host: str = 'localhost'
     port: int = 50052
 
-    async def get_financial_statement(self, symbol: str, count_of_years: int = 1, email: str | None = None):
+    async def get_financial_statement(self, schema: FinancialStatementRequestSchema):
         channel_opt = [('grpc.max_send_message_length', 512 * 1024 * 1024),
                        ('grpc.max_receive_message_length', 512 * 1024 * 1024)]
-        async with grpc.aio.insecure_channel(f'{self.host}:{self.port}', options=channel_opt, ) as channel:
+        async with grpc.aio.insecure_channel(f'{self.host}:{self.port}', options=channel_opt) as channel:
             stub = rpb2_grpc.FinancialStatementStub(channel=channel)
-            report_data = FinancialStatementRequestSchema(symbol=symbol, email=email)
-            message = rpb2.FinancialStatementRequest(**report_data.dict())
+            message = rpb2.FinancialStatementRequest(**schema.dict())
             document = await stub.GetFinancialStatement(message)
             return FinancialStatementResponseSchema(
-                symbol=document.symbol, pdf=gzip.decompress(document.pdf), year=document.year)
+                symbol=document.symbol,
+                pdf=gzip.decompress(document.pdf) if document.pdf else None,
+                year=document.year,
+                pdf_url=document.pdf_url
+            )
