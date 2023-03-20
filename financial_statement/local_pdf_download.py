420dd6710cbbd1b63254fe76d6135b860f6371fc
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-20 14:03:48 +0300

    modified local_pdf_download file

--- financial_statement/local_pdf_download.py
+++ financial_statement/local_pdf_download.py
@@ -2,18 +2,27 @@ import gzip
 from dataclasses import dataclass
 
 import aiofiles
+from aiofiles import os
+
+from protocols.file_service_protocol import FileProtocol
 
 
 @dataclass
-class PDFDownload:
-    folder: str
+class PDFDownload(FileProtocol):
+    folder: str = 'reports'
 
-    async def save_pdf(self, symbol: str, year: str, pdf: bytes):
+    async def save_pdf(self, filename: str, pdf: bytes):
         p = gzip.compress(pdf)
         async with aiofiles.open(
-                file=f'{self.folder}/{symbol}:{year}.pdf', mode='wb') as file:
+                file=f'{self.folder}/{filename}.pdf', mode='wb') as file:
             await file.write(p)
 
-    async def load_pdf(self, filename: str):
+    async def download_pdf(self, filename: str):
         async with aiofiles.open(file=f'{self.folder}/{filename}.pdf', mode='rb') as file:
             return await file.read()
+
+    async def delete_pdf(self, filename: str):
+        await os.remove(path=f'{self.folder}/{filename}.pdf')
+
+    async def is_file_exist(self, filename: str):
+        return await os.path.exists(path=f'{self.folder}/{filename}.pdf')
