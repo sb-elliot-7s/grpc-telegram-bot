8487726af8ddd63920851cd087fb24fcd0bc43aa
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-23 13:28:38 +0300

    some fix

--- users/kafka_service.py
+++ users/kafka_service.py
@@ -1,13 +1,8 @@
-import logging
-
-import orjson
 from aiokafka import AIOKafkaConsumer
 
-from configs import Topic
+from constants import Topic
 from repositories import UserRepositories
-from schemas import UserSchema, UserUpdateSchema
-
-logging.basicConfig(level=logging.DEBUG, filename='k.log')
+from schemas import UserSchema, UserUpdateSchema, KafkaSettingsSchema
 
 
 class KafkaService:
@@ -15,31 +10,26 @@ class KafkaService:
         self.repositories = repositories
         self.server: str
         self.topic = topic
-        self.consumer = AIOKafkaConsumer(
-            *self.topic,
-            bootstrap_servers=server,
-            group_id='user-group',
-            value_deserializer=lambda x: orjson.loads(x)
-        )
+        self.consumer = AIOKafkaConsumer(*self.topic, bootstrap_servers=server, **KafkaSettingsSchema().dict())
 
-    async def __handle_user_topic(self, message):
+    async def __handle_save_user_topic(self, message):
         if not await self.repositories.get_user(user_id=message.value.get('user_id')):
             user = UserSchema(**message.value)
             await self.repositories.save_user(user=user.to_mongo_obj())
 
     async def __handle_update_email_topic(self, message):
         user_email = UserUpdateSchema(**message.value)
-        await self.repositories.update_user(user_data=user_email)
+        await self.repositories.change_email(**user_email.dict())
 
     async def __handle_remove_email(self, message):
-        await self.repositories.remove_email(user_id=message.value['user_id'])
+        await self.repositories.change_email(user_id=message.value['user_id'], email=None)
 
     async def _handle_messages(self, message):
         match message.topic:
             case Topic.SAVE_EMAIL.value:
                 await self.__handle_update_email_topic(message=message)
             case Topic.USER.value:
-                await self.__handle_user_topic(message=message)
+                await self.__handle_save_user_topic(message=message)
             case Topic.REMOVE_EMAIL.value:
                 await self.__handle_remove_email(message=message)
 
@@ -47,7 +37,6 @@ class KafkaService:
         await self.consumer.start()
         try:
             async for message in self.consumer:
-                logging.debug('MESAGE: %s', message.value)
                 await self._handle_messages(message=message)
         finally:
             await self.consumer.stop()
