b63f80213b7b495d95826e480c16a4be8f33362f
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-23 13:11:16 +0300

    modified change_email() method

--- users/protocols/repositories_protocol.py
+++ users/protocols/repositories_protocol.py
@@ -1,7 +1,5 @@
 from abc import ABC, abstractmethod
 
-from schemas import UserUpdateSchema
-
 
 class UserRepositoriesProtocol(ABC):
     @abstractmethod
@@ -13,18 +11,12 @@ class UserRepositoriesProtocol(ABC):
         pass
 
     @abstractmethod
-    async def count_of_users(
-            self, filters: dict,
-            skip: int | None = None,
-            limit: int | None = None
-    ):
+    async def count_of_users(self, filters: dict, skip: int | None = None, limit: int | None = None):
         pass
 
     @abstractmethod
     async def save_user(self, user: dict): pass
 
     @abstractmethod
-    async def update_user(self, user_data: UserUpdateSchema): pass
-
-    @abstractmethod
-    async def remove_email(self, user_id: int): pass
+    async def change_email(self, user_id: int, email: str | None):
+        pass
