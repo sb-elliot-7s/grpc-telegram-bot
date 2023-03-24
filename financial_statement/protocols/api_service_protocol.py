036ae749678d9f024852e35ae4a9b8e1b4b04953
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-24 14:26:41 +0300

    add get_report() method

--- financial_statement/protocols/api_service_protocol.py
+++ financial_statement/protocols/api_service_protocol.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 
+import grpc
+
 
 class FinancialStatementServiceProtocol(ABC):
     @abstractmethod
     async def get_financial_statement_data(self, ticker: str):
         pass
+
+    @abstractmethod
+    async def get_report(self, symbol: str, context: grpc.aio.ServicerContext) -> dict: pass
