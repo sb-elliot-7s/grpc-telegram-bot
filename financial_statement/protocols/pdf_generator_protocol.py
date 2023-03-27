37551526590da068952f1bf12b2cee4645803639
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-27 15:39:26 +0300

    modified PDFGeneratorProtocol

--- financial_statement/protocols/pdf_generator_protocol.py
+++ financial_statement/protocols/pdf_generator_protocol.py
@@ -1,6 +1,20 @@
+import gzip
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+from protocols.file_service_protocol import FileProtocol
 
 
+@dataclass
 class PDFGeneratorProtocol(ABC):
+    url: str
+    filename: str
+    file_service: FileProtocol
+
     @abstractmethod
     async def generate_pdf(self) -> bytes: pass
+
+    async def execute(self) -> bytes:
+        pdf: bytes = await self.generate_pdf()
+        await self.file_service.save_pdf(filename=self.filename, pdf=pdf)
+        return gzip.compress(pdf)
