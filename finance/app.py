5b9be0a6f7401e058eeaf20f7027037ed43313e6
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-28 14:58:09 +0300

    delete commented code

--- finance/app.py
+++ finance/app.py
@@ -6,13 +6,8 @@ import finance_pb2_grpc as pb2_grpc
 from cache_finance_service import CacheFinanceService
 from configs import get_configs
 from finance_service_from_api import APIFinanceService
-# from finance_service_yahoo import FinanceService, CacheFinanceService
 from news_service import NewsService
 from servicer import FinanceServicer
-
-
-# def get_finance_service(debug: bool):
-#     return FinanceService() if debug else CacheFinanceService(service=FinanceService())
 
 
 def get_api_service(debug: bool):
@@ -21,7 +16,6 @@ def get_api_service(debug: bool):
 
 async def run_server():
     server = grpc.aio.server(compression=grpc.Compression.Gzip)
-    # finance_service = get_finance_service(debug=get_configs().debug)
     finance_service = get_api_service(debug=get_configs().debug)
     pb2_grpc.add_FinanceServicer_to_server(
         servicer=FinanceServicer(
