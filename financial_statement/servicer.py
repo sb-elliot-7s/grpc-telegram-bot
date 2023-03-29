0b279b12e607276602be7f263e2d33c5efeb2960
sb-elliot-7s <s.babichevv91p@gmail.com>
2023-03-29 12:57:51 +0300

    refactoring servicer

--- financial_statement/servicer.py
+++ financial_statement/servicer.py
@@ -1,71 +1,56 @@
-import os.path
+import logging
 
 import grpc
 
 import financial_statement_pb2 as rpb2
-import financial_statement_pb2_grpc as rp2_grpc
-from api_service import FinancialStatementService
+import financial_statement_pb2_grpc as rpb2_grpc
+from api_service import APIFinancialStatementService
 from command_client import CommandClient
-from configs import get_configs
-from email_command import EmailGrpcCommand
-from email_logic import EmailService
+from constants import TypeFileReturned
+from email_kafka_command import EmailKafkaCommand
+from email_kafka_receiver import EmailKafkaReceiver
+from file_service import FileService
 from grpc_command import GRPCCommand
-from grpc_service import GRPCReceiver
-from local_pdf_download import PDFDownload
+from grpc_receiver import GRPCReceiver
 from pdf_service import PdfGenerator
 from protocols.command_protocols import Command
-from schemas import EmailDataSchema
+from s3_storage import S3StorageFileService
+from schemas import KafkaEmailPayloadSchema, FilenameSchema
+from utils import Utils
+
+logging.basicConfig(level=logging.DEBUG, filename='b.log')
 
 
-class FinancialGRPCServicer(rp2_grpc.FinancialStatementServicer):
+class FinancialGRPCServicer(rpb2_grpc.FinancialStatementServicer, Utils):
+    CONTEXT = grpc.aio.ServicerContext
+    GRPC_REQUEST = rpb2.FinancialStatementRequest
 
-    async def GetFinancialStatement(self, request: rpb2.FinancialStatementRequest, context: grpc.aio.ServicerContext):
-        report = await self._get_report(request=request, context=context)
-        year = self.__parse_year(report_dict=report)
-        pdf: bytes = await self._get_pdf(symbol=request.symbol, year=year, link=report.get('finalLink'))
-        command = await self._get_command(context=context, request=request, pdf=pdf, year=year)
+    async def GetFinancialStatement(self, request: GRPC_REQUEST, context: CONTEXT):
+        report: dict = await APIFinancialStatementService().get_report(symbol=request.symbol, context=context)
+        logging.debug('REPORT: %s', report)
+        year: str = self.parse_year(report_dict=report)
+
+        pdf: str | bytes = await self._get_pdf(request=request, year=year, link=report.get('finalLink'))
+        command: Command = await self._get_command(context=context, req=request, pdf=pdf, year=year)
         return await CommandClient(command=command).process()
 
-    async def _get_command(
-            self, context: grpc.aio.ServicerContext, request: rpb2.FinancialStatementRequest, pdf: bytes,
-            year: str) -> Command | str:
-        grpc_command = GRPCCommand(
-            receiver=GRPCReceiver(context=context, pdf=pdf, symbol=request.symbol, year=year))
-        email_grpc_command = EmailGrpcCommand(command=grpc_command, email_service=EmailService(
-            **self.__get_email_data(to_user=request.email, pdf=pdf, text=request.symbol, year=year)))
-        return email_grpc_command if request.email else grpc_command
-
     @staticmethod
-    def __get_email_data(text: str, pdf: bytes, to_user: str, year: str) -> dict:
-        return EmailDataSchema(text=text, pdf=pdf, year=year, to_user=to_user).dict()
+    async def _get_pdf(request: GRPC_REQUEST, year: str, link: str) -> bytes | str:
+        file_service = S3StorageFileService(type_file_returned=TypeFileReturned.URL)
+        filename: str = FilenameSchema(symbol=request.symbol, year=year).filename
+        return await FileService(
+            file_service=file_service,
+            pdf_resp=request.pdf_response,
+            filename=filename,
+            pdf_service=PdfGenerator(file_service=file_service, filename=filename, url=link)
+        ).execute()
 
-    @staticmethod
-    def __parse_year(report_dict: dict) -> str:
-        # 2022-10-27 18:01:14
-        return report_dict.get('acceptedDate').split()[0].split('-')[0]
-
-    @staticmethod
-    async def __generate_pdf(symbol: str, link: str, year: str) -> bytes:
-        return await PdfGenerator().main(url=link, symbol=symbol, year=year)
-
-    @staticmethod
-    async def __load_pdf(symbol: str, year: str) -> bytes:
-        return await PDFDownload(folder='reports').load_pdf(filename=f'{symbol}:{year}')
-
-    @staticmethod
-    def __if_file_exist(symbol: str, year: str) -> bool:
-        return os.path.exists(path=f'reports/{symbol}:{year}.pdf')
-
-    async def _get_pdf(self, symbol: str, year: str, link: str) -> bytes:
-        return await self.__load_pdf(symbol=symbol, year=year) \
-            if self.__if_file_exist(symbol=symbol, year=year) \
-            else await self.__generate_pdf(symbol=symbol, link=link, year=year)
-
-    @staticmethod
-    async def _get_report(request: rpb2.FinancialStatementRequest, context: grpc.aio.ServicerContext) -> dict:
-        last_report = 0
-        reports: list[dict] = await FinancialStatementService(base_url=get_configs().base_url) \
-            .get_financial_statement_data(ticker=request.symbol)
-        if not len(reports):
-            await context.abort(code=grpc.StatusCode.INVALID_ARGUMENT, details='You must pass valid symbol')
-        return reports[last_report]
+    async def _get_command(self, context: CONTEXT, req: GRPC_REQUEST, pdf: bytes | str, year: str) -> Command:
+        grpc_command = GRPCCommand(receiver=GRPCReceiver(context=context, pdf=pdf, symbol=req.symbol, year=year))
+        data = KafkaEmailPayloadSchema(
+            filename=FilenameSchema(symbol=req.symbol, year=year).filename,
+            to_user=req.email,
+            link=self.get_pdf_url_or_none(pdf=pdf)
+        )
+        kafka_email_command = EmailKafkaCommand(command=grpc_command, receiver=EmailKafkaReceiver(**data.dict()))
+        return kafka_email_command if req.email else grpc_command
